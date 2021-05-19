57dd57dbeb0a5c051c31df7edd575861c8a7e3da
1000119577 <soumyadipg04@gmail.com>
2021-05-20 02:34:47 +0530

    moderate styling done

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View, TextInput, TouchableOpacity, Vibration } from '
 
 export default class App extends React.Component {
   constructor() {
-    super() 
+    super()
     this.state = {
       time: 25 * 60,
       status: 'working',
@@ -13,7 +13,7 @@ export default class App extends React.Component {
       breakTime: 5 * 60
     }
   }
-  
+
   handleChange = (text) => {
     this.setState({
       time: text * 60,
@@ -21,7 +21,7 @@ export default class App extends React.Component {
   }
 
   timerStart = () => {
-    if (!this.state.running){
+    if (!this.state.running) {
       this.setState({
         running: true,
         butt: 'pause'
@@ -31,50 +31,50 @@ export default class App extends React.Component {
           time: this.state.time - 1
         })
         if (this.state.time === 0 && this.state.status === 'working') {
-            this.setState({
-              running: false,
-              status: 'chilling',
-              time: this.state.breakTime
-            })
-            clearInterval(this.id)
-            Vibration.vibrate([500,500,500])
-            this.timerStart()
-        }        
+          this.setState({
+            running: false,
+            status: 'chilling',
+            time: this.state.breakTime
+          })
+          clearInterval(this.id)
+          Vibration.vibrate([500, 500, 500])
+          this.timerStart()
+        }
         else if (this.state.status === 'chilling' && this.state.time === 0) {
           clearInterval(this.id)
           this.setState({
-              time: 25 * 60,
-              butt: 'start',
-              status: 'working'
-            })
+            time: 25 * 60,
+            butt: 'start',
+            status: 'working'
+          })
         }
       }, 1000)
     }
     else {
       clearInterval(this.id)
       this.setState({
-          running: false,
-          butt: 'resume'
-        })
+        running: false,
+        butt: 'resume'
+      })
     }
   }
 
   reset = () => {
     clearInterval(this.id)
     this.setState({
-      time: 25*60,
+      time: 25 * 60,
       running: false,
       status: 'working',
       butt: 'start'
     })
   }
-  
+
   handleBreakChange = (text) => {
     this.setState({
       breakTime: text * 60
     })
-  } 
-  
+  }
+
   pad(d) {
     return (d < 10) ? '0' + d.toString() : d.toString();
   }
@@ -82,24 +82,30 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.text}>
-          {this.pad(Math.floor(this.state.time/60)) + ':' + this.pad(this.state.time % 60)}
-        </Text>
-        <TextInput keyboardType='numeric' defaultValue={'' + 25} onChangeText={this.handleChange} />
-        <TextInput keyboardType='numeric' defaultValue={'' + 5} onChangeText={this.handleBreakChange} />
-        <TouchableOpacity onPress={this.timerStart}>
-          <Text>
-            {this.state.butt}
+        <View style={styles.timer}>
+          <Text style={{ fontSize: 40 }}>
+            {this.state.status}
           </Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={this.reset}>
-          <Text>
-            reset
+          <Text style={styles.text}>
+            {this.pad(Math.floor(this.state.time / 60)) + ':' + this.pad(this.state.time % 60)}
           </Text>
-        </TouchableOpacity>
-        <Text>
-          {this.state.status}
-        </Text>
+        </View>
+        <View style={styles.input}>
+          <TextInput style={{ margin: 10, fontSize: 30 }} keyboardType='numeric' defaultValue={'' + 25} onChangeText={this.handleChange} />
+          <TextInput style={{ margin: 10, marginLeft: 50, fontSize: 30 }} keyboardType='numeric' defaultValue={'' + 5} onChangeText={this.handleBreakChange} />
+        </View>
+        <View style={styles.button}>
+          <TouchableOpacity style={styles.touch} onPress={this.timerStart}>
+            <Text style={styles.tt}>
+              {this.state.butt}
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.touch} onPress={this.reset}>
+            <Text style={styles.tt}>
+              reset
+          </Text>
+          </TouchableOpacity>
+        </View>
       </View>
     )
   }
@@ -107,12 +113,29 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    marginTop:100,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    flexDirection: 'column',
+    alignItems: 'center',
+    flex: 1,
   },
   text: {
-    fontSize: 20,
+    fontSize: 40,
+    marginLeft: 15
   },
+  input: {
+    flexDirection: 'row',
+  },
+  timer: {
+    margin: 25,
+  },
+  button: {
+    flexDirection: 'row',
+  },
+  touch: {
+    margin: 10
+  },
+  tt: {
+    fontSize: 30,
+  }
 });
